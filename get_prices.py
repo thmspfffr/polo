510b6aa1b248da44bed4ff1a32b49240d4e80dd9
thmspfffr <thpfeffer@gmx.de>
2017-09-11 10:34:16 +0200

    new get prices function

---
+++ get_prices.py
@@ -0,0 +1,47 @@
+from coinmarketcap import Market
+import pandas as pd
+import time
+import numpy as np
+import os
+
+def write_prices():
+
+    coinmarketcap = Market()
+
+    coins = ['ARDOR','basic-attention-token','IOTA','lisk','nem']
+
+    val = np.zeros(5)
+    icoin = 0
+    t = int(time.time())
+
+    for coin in coins:
+
+        val[icoin] = float(coinmarketcap.ticker(coin, limit=3, convert='USD')[0]['price_usd'])
+        icoin = icoin + 1
+
+    labels = ['ARDR','BAT','IOTA','LSK','XEM']
+
+    df = pd.DataFrame(data=[val] ,index=[t],columns=labels)
+    df.index.name = 'Timestamp'
+
+    if os.path.isfile('output/prices.csv'):
+        with open('output/prices.csv', 'a') as f:
+            df.to_csv(f,header=False)
+
+    else:
+        with open('output/prices.csv', 'w') as f:
+            df.to_csv(f)
+
+#try:
+
+while True:
+    print("Writing current USD prices to CSV...")
+    write_prices()
+    time.sleep(30)
+
+#except:
+#    print("ERROR")
+
+
+
+
--- plot.py
+++ plot.py
@@ -33,6 +33,58 @@ def poloplot():
                
     ftp.storbinary('STOR mybalance.png', open('mybalance.png','rb')  )  
     ftp.quit()
+    
+def bar_plot(timescale):
+    """
+    Makes bar plot, containing the hourly/daily/weekly/all-time changes
+    of all currencies that are currently held
+    Timescale: 'percent_change_1h','percent_change_24h','percent_change_7d'
+    """
+    all_col = ['r','g']
+    currencies = ['ARDOR','basic-attention-token','IOTA','LISK','NEM']
+    delta_price = list()
+    colors = list()
+
+    for icurr in currencies:
+
+        val = float(coinmarketcap.ticker(icurr, limit=3, convert='USD')[0][timescale])
+        delta_price.append(val)
+        colors.append(all_col[int(val>0)])
+
+    tl = ['ARDR','BAT','IOTA','LSK','XEM']
+    fig, ax = plt.subplots()
+    fig.set_facecolor((1, 1, 1))
+    ax.bar([0.5,2,3.5,5,6.5],delta_price,width=1,color=colors,linewidth=0)
+
+    ax.set_ylabel('Change [%]')
+    ax.set_xticks([.5,2,3.5,5,6.5])
+    ax.set_xticklabels((tl))
+
+    if np.max(np.abs(delta_price)) < 5:
+        ax.set_ylim([-5,5])
+    elif np.max(np.abs(delta_price)) < 10 and np.max(np.abs(delta_price)) > 5:
+        ax.set_ylim([-10,10])
+    elif np.max(np.abs(delta_price)) < 25 and np.max(np.abs(delta_price)) > 10:
+        ax.set_ylim([-25,25])
+    elif np.max(np.abs(delta_price)) < 50 and np.max(np.abs(delta_price)) > 25:
+        ax.set_ylim([-50,50])
+    elif np.max(np.abs(delta_price)) < 100 and np.max(np.abs(delta_price)) > 50:
+        ax.set_ylim([-100,100])
+    else:
+        ax.set_ylim([-200,200])
+
+    ax.grid('on',axis='y')
+    ax.set_title('%s' % timescale)
+    plt.savefig('plots/bar_plot_%s.png' % timescale)
+    plt.close()
+
+    ftp = FTP('s610.deinprovider.de')
+    ftp.login('web10','IzULi9HE')
+    ftp.cwd('html')
+
+    ftp.storbinary('STOR bar_plot_%s.png' %timescale, open('plots/bar_plot_%s.png' %timescale,'rb'))  
+    ftp.quit()
+    
 
 def make_html(totalPercentChange,totalValue):
 
@@ -45,29 +97,16 @@ def make_html(totalPercentChange,totalValue):
     else:
         file.write('<P><b>Total percent change:</b> %.2f%%' % totalPercentChange) 
 
-    file.write('<P><b>ARDR</b>, \t1h: %.2f%% ' % float(coinmarketcap.ticker('ARDOR', limit=3, convert='USD')[0]['percent_change_1h'])) 
-    file.write('\t1d: %.2f%% / ' % float(coinmarketcap.ticker('ARDOR', limit=3, convert='USD')[0]['percent_change_24h'])) 
-    file.write('\t7d: %.2f%%\n' % float(coinmarketcap.ticker('ARDOR', limit=3, convert='USD')[0]['percent_change_7d'])) 
+    #file.write('<br><br><IMG src="mybalance.png" height="400" width="500">') 
 
-    file.write('<P><b>BAT</b>, \t1h: %.2f%% ' % float(coinmarketcap.ticker('basic-attention-token', limit=3, convert='USD')[0]['percent_change_1h'])) 
-    file.write('\t1d: %.2f%% ' % float(coinmarketcap.ticker('basic-attention-token', limit=3, convert='USD')[0]['percent_change_24h'])) 
-    file.write('\t7d: %.2f%%\n' % float(coinmarketcap.ticker('basic-attention-token', limit=3, convert='USD')[0]['percent_change_7d'])) 
+    bar_plot('percent_change_1h')
+    file.write('<br>')
+    file.write('<br><IMG src="bar_plot_percent_change_1h.png" height="300" width="400">')  
+    bar_plot('percent_change_24h')
+    file.write('<IMG src="bar_plot_percent_change_24h.png" height="300" width="400">') 
+    bar_plot('percent_change_7d')
+    file.write('<IMG src="bar_plot_percent_change_7d.png" height="300" width="400">') 
 
-    file.write('<P><b>IOTA</b>, \t1h: %.2f%% ' % float(coinmarketcap.ticker('IOTA', limit=3, convert='USD')[0]['percent_change_1h'])) 
-    file.write('\t1d: %.2f%% ' % float(coinmarketcap.ticker('IOTA', limit=3, convert='USD')[0]['percent_change_24h'])) 
-    file.write('\t7d: %.2f%%\n' % float(coinmarketcap.ticker('IOTA', limit=3, convert='USD')[0]['percent_change_7d'])) 
-
-    file.write('<P><b>LISK</b>, \t1h: %.2f%% ' % float(coinmarketcap.ticker('LISK', limit=3, convert='USD')[0]['percent_change_1h'])) 
-    file.write('\t1d: %.2f%% ' % float(coinmarketcap.ticker('LISK', limit=3, convert='USD')[0]['percent_change_24h'])) 
-    file.write('\t7d: %.2f%%\n' % float(coinmarketcap.ticker('LISK', limit=3, convert='USD')[0]['percent_change_7d'])) 
-
-    file.write('<P><b>NEM</b>, \t1h: %.2f%% ' % float(coinmarketcap.ticker('nem', limit=3, convert='USD')[0]['percent_change_1h'])) 
-    file.write('\t1d: %.2f%% ' % float(coinmarketcap.ticker('nem', limit=3, convert='USD')[0]['percent_change_24h'])) 
-    file.write('\t7d: %.2f%%\n' % float(coinmarketcap.ticker('nem', limit=3, convert='USD')[0]['percent_change_7d'])) 
-
-
-
-    file.write('<br><br><IMG src="mybalance.png" height="400" width="500">')  
     file.write('</BODY></HTML>')  
 
     file.close()
